4b890cf0b1cb20cf6a4b0042ba7eede4e35cc5ad
dhana <dl24.dhana@gmail.com>
2021-05-22 18:46:52 +0530

    HIS-345 fixed

--- src/main/java/com/ashokit/SpringbootApp1Application.java
+++ src/main/java/com/ashokit/SpringbootApp1Application.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.ashokit.beans.Car;
+
 @SpringBootApplication
 public class SpringbootApp1Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(SpringbootApp1Application.class, args);
+		Car c=new Car();
+		c.generateEngine();
+		
 		context.close();
 	}
 
