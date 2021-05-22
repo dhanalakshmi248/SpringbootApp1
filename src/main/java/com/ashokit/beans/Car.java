f52379e1750afe03a59daee29026f639685940a2
dhana <dl24.dhana@gmail.com>
2021-05-22 18:49:43 +0530

    HIS-263 fixed

--- src/main/java/com/ashokit/beans/Car.java
+++ src/main/java/com/ashokit/beans/Car.java
@@ -1,9 +1,16 @@
 package com.ashokit.beans;
 
-public class Car {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Car {
+	@Autowired
+private Engine engine;
 	public void generateEngine()
 	{
 		System.out.println("generate engine for car...");
+		engine.providingEngine();
+		//changes for HIS-263
 	}
+	
+	
 }
